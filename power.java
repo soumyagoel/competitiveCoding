ab1dbf0e1dd60c30865b09c9f7d215b0cb2a24d6
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-07-20 22:33:38 +0530

    power

---
+++ power.java
@@ -0,0 +1,36 @@
+package june7;
+
+import java.util.Scanner;
+
+
+public class power {
+	public static void main(String[] args)
+	{
+		Scanner scn=new Scanner(System.in);
+		System.out.println("enter one number");
+		int x=scn.nextInt();
+		System.out.println("enter another number");
+		int n=scn.nextInt();
+		int pow,count,log;
+		pow=1;
+		count=1;
+		while(count<=n)
+		{
+			pow=pow*x;
+			count=count+1;
+		}
+		System.out.println(pow);
+		System.out.println("enter one number");
+		int a=scn.nextInt();
+		System.out.println("enter another number");
+		int b=scn.nextInt();
+		log=0;
+		while(a!=1)
+		{
+			a=a/b;
+			log=log+1;
+		}
+		System.out.println("\n"+log);
+	}
+
+}
