b48a41326de90475c2c8e1ab6b9823eb2348fd48
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-07-20 22:29:11 +0530

    oddeven

---
+++ oddeven.java
@@ -0,0 +1,30 @@
+package june7;
+
+import java.util.Scanner;
+
+public class oddeven {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.print("enter a number");
+		int n = scn.nextInt();
+		int rem, a, b, c;
+		a = 0;
+		b = 0;
+		c = 1;
+		while (n != 0) {
+			rem = n % 10;
+			if (c % 2 == 0) {
+				a = a + rem;
+			}
+			if (c % 2 != 0) {
+				b = b + rem;
+			}
+			c = c + 1;
+			n = n / 10;
+		}
+		System.out.println(b);
+		System.out.println(a);
+		
+	}
+
+}
