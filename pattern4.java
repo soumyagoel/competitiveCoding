160dd407edde102b943aadbfe33965f1ddef1a31
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-07-20 22:31:29 +0530

    pattern

---
+++ pattern4.java
@@ -0,0 +1,27 @@
+package june7;
+
+import java.util.Scanner;
+
+
+public class pattern4 {
+	public static void main(String[] args)
+	{
+		Scanner scn = new Scanner(System.in);
+		System.out.println("enter a number");
+		int n=scn.nextInt();
+		int row,col,val;
+		row=1;
+		val=1;
+		while(row<=n)
+		{col=1;
+			while(col<=row)
+			{
+				System.out.print(val);
+				col=col+1;
+			}
+			System.out.println();
+			row=row+1;
+		}
+	}
+
+}
