f4b30002c825c4c092b49a35589f624d996a0739
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-07-20 22:34:33 +0530

    prime

---
+++ prime1.java
@@ -0,0 +1,34 @@
+package june7;
+
+import java.util.Scanner;
+
+
+public class prime1 {
+	public static void main(String[] args)
+	{
+		Scanner scn=new Scanner(System.in);
+		System.out.println("enter the one number");
+		int n1=scn.nextInt();
+		System.out.println("enter another number");
+		int n2=scn.nextInt();
+		int count;
+		while(n1<=n2)
+		{
+			count=2;
+		     while(count<n1)
+		     {
+		    	 if(n1%count==0)
+		    	 {
+		    		 break;
+		    	 }
+		    	 count++;
+		     }
+		     if(n1==count)
+		     {
+		    	 System.out.println(n1);
+		     }
+		     n1++;
+		}
+	}
+
+}
