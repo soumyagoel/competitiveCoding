100975d96c42df3bdf1a0254149f7a20d8aa2ce1
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-07-20 22:34:08 +0530

    prime

---
+++ prime.java
@@ -0,0 +1,30 @@
+package june7;
+
+import java.util.Scanner;
+
+
+public class prime {
+	public static void main(String[] args)
+	{
+		Scanner scn=new Scanner(System.in);
+		System.out.println("enter a number");
+		int n=scn.nextInt();
+		int count =2;
+		while(count<n)
+		{
+			if(n%count==0)
+			{
+				System.out.print("not prime ");
+				return;
+			}
+			else
+			{
+				count=count+1;
+			
+			}
+		}
+			System.out.println("prime");
+			
+	}
+
+}
