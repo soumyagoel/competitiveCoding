93f21f108a776fcba565ac20f9ac099926da553d
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-07-21 23:17:35 +0530

    conversion

---
+++ p2.java
@@ -0,0 +1,24 @@
+package june9;
+
+import java.util.Scanner;
+
+public class p2 {
+	public static void main(String[] args)
+	{
+		Scanner scn=new Scanner(System.in);
+		System.out.println("enter the number");
+		int n=scn.nextInt();
+		int pow,dec,rem;
+		dec=0;
+		pow=1;
+		while(n!=0)
+		{
+			rem=n%10;
+			n=n/10;
+			dec=dec+rem*pow;
+			pow=2*pow;
+		}
+		System.out.println(dec);
+	}
+
+}
