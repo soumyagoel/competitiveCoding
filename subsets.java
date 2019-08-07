19f2aa4e7d6e4afb71ceaf3b35094307aa7368fc
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-08-07 10:24:27 +0530

    subsets

---
+++ subsets.java
@@ -0,0 +1,35 @@
+package june14;
+import java.util.Scanner;
+public class subsets 
+{
+	/**
+	 * @param args
+	 */
+	public static Scanner scn=new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr={1,2,3};              
+		subs(arr);
+	}
+	public static void subs(int[] arr)
+	{
+		int t=(int)Math.pow(2,arr.length);                     //	subsets
+		int count=0;
+		while(count<t)
+		{
+			System.out.print("[");
+			int temp=count;
+			for(int i=0;i<arr.length;i++)
+			{
+			int rem=temp%2;
+			if(rem==1)
+			{
+				System.out.print(arr[i]+" , ");
+			}
+			temp=temp/2;
+			}
+			count++;
+		    System.out.print("]");
+		}
+	}
+}
