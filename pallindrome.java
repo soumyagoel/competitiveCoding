54d8edd887f60940daae72ee2134e5a0341ce140
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-08-14 10:10:06 +0530

    Add files via upload

---
+++ pallindrome.java
@@ -0,0 +1,25 @@
+package june19;
+
+public class pallindrome 
+{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a=ispallindrome("hello");
+		System.out.println(a);
+	}
+	public static boolean ispallindrome(String s)
+	{
+		int j=s.length()-1;;
+		for(int i=0;i<=s.length()/2;i++)
+		{
+			if(s.charAt(i)!=s.charAt(j)){
+				return false;
+			}
+			j--;
+		}
+		return true;
+	}
+}
