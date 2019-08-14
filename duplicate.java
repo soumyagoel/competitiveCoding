5337ae789f2a642ff0cf50b7ed88743f5f80d725
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-08-14 10:11:28 +0530

    Add files via upload

---
+++ duplicate.java
@@ -0,0 +1,29 @@
+package june19;
+
+public class duplicate {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder ab=new StringBuilder("aaaabbca");
+		duplicate(ab);
+
+	}
+	public static void duplicate(StringBuilder sb)
+	{
+		StringBuilder s =new StringBuilder();
+		for(int i=0;i<sb.length()-1;i++)
+		{
+			if(sb.charAt(i)!=sb.charAt(i+1))
+			{
+				s.append(sb.charAt(i));
+			}
+			
+		}
+			s.append(sb.charAt(sb.length()-1));
+			System.out.print(s);		
+	}
+
+}
