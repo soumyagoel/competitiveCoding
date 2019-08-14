0be8fe81a210608a67821b12f5a52c35c74f1a1d
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-08-14 10:23:04 +0530

    Add files via upload

---
+++ togglecase.java
@@ -0,0 +1,33 @@
+package june19;
+
+public class togglecase {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder ab=new StringBuilder("aaaADbBcCda");
+		toggle(ab);     
+	}
+	public static void toggle(StringBuilder sb)
+	{
+		StringBuilder s=new StringBuilder();
+		for(int i=0;i<sb.length();i++)
+		{
+			char ch=sb.charAt(i);
+			char nch='\0';
+			if(ch>='a'&&ch<='z')
+			{
+				nch=(char)(ch-'a'+'A');
+			}
+			else
+				if(ch>='A'&&ch<='Z')
+				{
+					nch=(char)(ch-'A'+'a');
+				}
+			s.append(nch);
+		}
+		System.out.print(s);
+	}
+}
