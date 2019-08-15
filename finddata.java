6b805f4c8b55734d99a52c62a313ce83b65cfa6c
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-08-15 22:15:37 +0530

    Add files via upload

---
+++ finddata.java
@@ -0,0 +1,35 @@
+package june21;
+
+import java.util.Scanner;
+
+public class finddata {
+	/**
+	 * @param args
+	 */
+	public static Scanner scn=new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr={55,11,44,22,33};
+		System.out.println("enter the number");
+		int a=scn.nextInt();
+		boolean b=find(arr,0,a);
+		System.out.println(b);
+        
+	}
+	public static boolean find(int[] arr,int vidx,int a)
+	{
+		if(vidx==arr.length)
+		{
+			return false;
+		}
+//		find(arr,vidx+1,a);
+		if(arr[vidx]==a)
+		{
+			return true;
+		}
+		boolean ans = find(arr,vidx+1,a);
+		return ans;
+		
+	}
+
+}
