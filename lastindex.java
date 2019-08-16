8f73237b1ce3040f5f665ff27cb697c28bf9b22b
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-08-16 21:47:26 +0530

    Add files via upload

---
+++ lastindex.java
@@ -0,0 +1,34 @@
+package june21;
+
+import java.util.Scanner;
+
+public class lastindex {
+
+	/**
+	 * @param args
+	 */
+	public static Scanner scn=new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr={3,7,4,7,7};
+		System.out.println("enter the number");
+		int b=scn.nextInt();
+		int a=last(arr,arr.length-1,b);
+		System.out.println(a);
+
+	}
+	public static int last(int[] arr,int vidx,int b)
+	{
+		if(vidx==-1)
+		{
+			return -1;
+		}
+		if(b==arr[vidx])
+		{
+			return vidx;
+		}
+		int ans=last(arr,vidx-1,b);
+		return ans;
+	}
+
+}
