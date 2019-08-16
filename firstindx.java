2923836041ce17d49d3ec40fe508210e4e8b4c91
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-08-16 21:46:21 +0530

    Add files via upload

---
+++ firstindx.java
@@ -0,0 +1,33 @@
+package june21;
+
+import java.util.Scanner;
+
+public class firstindx {
+
+	/**
+	 * @param args
+	 */
+	public static Scanner scn=new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int[] arr={3,7,4,7,7};
+       System.out.println("enter the number");
+       int b=scn.nextInt();
+       int a=first(arr,0,b);
+       System.out.println(a);
+	}
+	public static int first(int[] arr,int vidx,int b)
+	{
+		if(vidx==arr.length)
+		{
+			return Integer.MIN_VALUE;
+		}
+		if(b==arr[vidx])
+		{
+			return vidx;
+		}
+		int ans=first(arr,vidx+1,b);
+		return ans;
+	}
+
+}
