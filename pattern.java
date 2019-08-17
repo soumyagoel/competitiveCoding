754270e180034b1df1b7e9a71c5a5510c6b518da
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-08-17 11:18:00 +0530

    Add files via upload

--- pattern.java
+++ pattern.java
@@ -1,42 +1,32 @@
-
-import java.util.Scanner;
-
+package june21;
 
 public class pattern {
-	public static void main(String[] args)
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		pat(0,0,6,1);
+
+	}
+	public static void pat(int row,int col,int n,int val)
 	{
-		Scanner scn=new Scanner(System.in);
-		System.out.println("Enter a number");
-		int n=scn.nextInt();
-		int row=1,nsp,nst=1;
-		nsp=n/2;
-		while(row<=n)
+		if(row>n-1)
 		{
-			int csp=1;
-			while(csp<=nsp)
-			{
-				System.out.print(" ");
-				csp=csp+1;
-			}
-			int cst=1;
-			while(cst<=nst)
-			{
-				System.out.print("*");
-				cst=cst+1;
-			}
-			if(row<=n/2)
-			{
-				nst=nst+2;
-				nsp=nsp-1;
-			}
-			else
-			{
-				nst=nst-2;
-				nsp=nsp+1;
-			}
-			System.out.println();
-			row=row+1;
+			return;
 		}
+		if(col>row)
+		{
+			System.out.println();
+			pat(row+1,0,n,1);
+			return;
+		}
+		System.out.print(val+"\t");
+		val=(val*(row-col))/(col+1);
+			pat(row,col+1,n,val);
+		}
+		
 	}
 
-}
+
