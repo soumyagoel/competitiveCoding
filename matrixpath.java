4629106b12697b36a58af91a156f41bde556e1db
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-08-24 23:48:05 +0530

    Add files via upload

---
+++ matrixpath.java
@@ -0,0 +1,38 @@
+package june23;
+
+import java.util.ArrayList;
+
+public class matrixpath {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> inter = matrix(0, 0, 2, 2);
+		System.out.println(inter);
+	}
+
+	public static ArrayList<String> matrix(int crow, int ccol, int erow,
+			int ecol) {
+		if (crow == erow && ccol == ecol) {
+			ArrayList<String> br = new ArrayList<String>();
+			br.add("");
+			return br;
+		}
+		if (crow > erow || ccol > ecol) {
+			ArrayList<String> br = new ArrayList<String>();
+			return br;
+		}
+		ArrayList<String> mr = new ArrayList<String>();
+		ArrayList<String> rrh = matrix(crow, ccol + 1, erow, ecol);
+		for (String rs : rrh) {
+			mr.add("H" + rs);
+		}
+		ArrayList<String> rrv = matrix(crow + 1, ccol, erow, ecol);
+		for (String rs : rrv) {
+			mr.add("V" + rs);
+		}
+
+		return mr;
+	}
+}
