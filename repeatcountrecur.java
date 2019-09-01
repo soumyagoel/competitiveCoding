5a0e60f0ef6bfb9a287a37727b1e61f7c1e4ec74
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-09-02 01:54:56 +0530

    Add files via upload

---
+++ repeatcountrecur.java
@@ -0,0 +1,43 @@
+package june26;
+
+import java.util.Scanner;
+
+public class repeatcountrecur {
+
+	/**
+	 * @param args
+	 */
+	public static Scanner scn=new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s=scn.next();
+		int s1=count(s);
+		System.out.println(s1);
+	}
+	public static int count(String str)
+	{
+		if(str.length()==0)
+		{
+			return 0;
+		}
+		int c=0;
+		for(int i=0;i<str.length()-2;i+=2)
+		{
+			char ch=str.charAt(i);
+			char ch1=str.charAt(i+1);
+			char ch2=str.charAt(i+2);
+			String s3=str.substring(i+2);
+			if(ch=='a'&&ch1=='a'&&ch2=='a')
+			{
+				c++;
+			}
+			else
+			{
+				continue;
+			}
+			count(s3);
+		}
+		return c;
+	}
+
+}
