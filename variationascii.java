e9ca250698d22ee3b26b1c24d067fdfc950bad79
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-09-05 23:35:36 +0530

    Add files via upload

---
+++ variationascii.java
@@ -0,0 +1,24 @@
+package june26;
+
+public class variationascii {
+     /**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ascii("ab","");
+	}
+	public static void ascii(String ques,String ans)
+	{
+		if(ques.length()==0)
+		{
+			System.out.println(ans);
+			return;
+		}
+		char ch=ques.charAt(0);
+		String roq=ques.substring(1);
+		ascii(roq,ans+"");
+		ascii(roq,ans+ch);
+		ascii(roq,ans+(int)ch);
+	}
+}
