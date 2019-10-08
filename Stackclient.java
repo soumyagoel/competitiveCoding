862f08b9698e3875dfaba83af70c0ef7ac10daec
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-10-08 10:00:02 +0530

    Add files via upload

---
+++ Stackclient.java
@@ -0,0 +1,30 @@
+package july12;
+
+public class Stackclient
+{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+//		constructor ab=new constructor(10);
+		constructor ab=new dynamicstack();
+		ab.push(10);
+		ab.push(20);
+		ab.push(30);
+		ab.push(40);
+//		ab.display();
+		ab.push(50);
+		ab.display();
+		ab.pop();
+//		ab.display();
+		ab.pop();
+		ab.pop();
+		ab.pop();
+		
+		System.out.println(ab.top());
+		System.out.println(ab.size());
+		System.out.println(ab.isempty());
+	}
+
+}
