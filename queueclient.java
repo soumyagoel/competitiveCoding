70f64cbd0981dc44a6eb1bb12b94abc0fbbdeb49
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-10-10 04:10:37 +0530

    Add files via upload

---
+++ queueclient.java
@@ -0,0 +1,27 @@
+package july17;
+
+public class queueclient {
+
+	public static void main(String[] args)throws Exception {
+		queue qe= new dynamicqueue();
+//		queue qe=new queue();
+		qe.enqueue(10);
+		qe.display();
+		qe.enqueue(20);
+		qe.display();
+		qe.enqueue(30);
+		qe.display();
+		qe.enqueue(40);
+		qe.display();
+		System.out.println(qe.dequeue());
+		qe.display();
+		System.out.println(qe.dequeue());
+		qe.display();
+		qe.enqueue(60);
+		qe.display();
+		qe.enqueue(70);
+		qe.display();		
+		System.out.println(qe.dequeue());
+        qe.display();
+	}
+}
