d1016ed71a83ad792556c7727ed8c06e7fb3f126
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-10-11 14:45:22 +0530

    Add files via upload

---
+++ client.java
@@ -0,0 +1,67 @@
+package july18;
+
+public class client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)throws Exception {
+		// TODO Auto-generated method stub
+		linkedlist ll=new linkedlist();
+		ll.AddLast(9);
+		ll.AddLast(10);
+		ll.AddLast(2);
+		ll.AddLast(50);
+//		ll.display();
+		ll.AddLast(35);
+//		ll.display();
+		ll.AddLast(5);
+		ll.display();
+		
+		ll.hasCycle();
+//		ll.insertionsort() ;
+//		ll.display();
+//		ll.AddFirst(100);
+//		ll.display();
+//		ll.addAt(1000,3);
+//		ll.display();
+//		ll.removeAt(3);
+//		ll.display();
+//		System.out.println(ll.size());
+//		ll.removeLast();
+//		ll.display();
+//		ll.removeLast();
+//		ll.display();
+//		ll.removeFirst();
+//		ll.display();
+//		ll.reversedataIterative();
+//		ll.display();
+//		ll.reverseIterativePointer();
+//		ll.display();
+//		ll.reversePointerRecursive();
+//		ll.display();
+//		ll.reverseDataRecursive();
+//		ll.display();
+//		ll.isPallindrome();
+//		ll.fold();
+//		ll.display();
+//		System.out.println(ll.kthfromLast(3));
+//		System.out.println(ll.midelement());
+//		ll.removeDuplicate();
+//		ll.display();
+//		ll=ll.mergesort();
+//		ll.display();
+//		ll.kreverse(2);
+//		ll.display();
+//		ll.swaptwoelements(0, 2);
+//		ll.display();
+//		ll=ll.oddeven();
+//		ll.display();
+//		ll.bubblesort();
+//		ll.display();
+//		ll.selectionsort();
+//		ll.display();
+//		
+	}
+
+}
