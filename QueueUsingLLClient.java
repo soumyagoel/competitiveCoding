dabbbd102e110e8d157d2825eec4376dcc29abeb
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-10-12 16:06:59 +0530

    Add files via upload

---
+++ QueueUsingLLClient.java
@@ -0,0 +1,25 @@
+package july18;
+
+public class QueueUsingLLClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		queueUsingLinkedList l2=new queueUsingLinkedList();
+		l2.enqueue(10);
+		l2.display();
+		l2.enqueue(20);
+		l2.display();
+		l2.enqueue(30);
+		l2.display();
+		l2.enqueue(40);
+		l2.display();
+		l2.dequeue();
+		l2.display();
+		System.out.println(l2.front());
+
+	}
+
+}
