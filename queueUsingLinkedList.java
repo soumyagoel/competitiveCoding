6b00ddc83d10c39fca0fe8f959bd6a4880c8146f
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-10-12 16:05:57 +0530

    Add files via upload

---
+++ queueUsingLinkedList.java
@@ -0,0 +1,33 @@
+package july18;
+
+public class queueUsingLinkedList {
+	linkedlist l1=new linkedlist();
+	public void enqueue(int value)
+	{
+		l1.AddLast(value);
+	}
+	public int dequeue()
+	{
+		try
+		{
+			return l1.removeFirst();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			return e.hashCode();
+		}
+	}
+	public int front()
+	{
+		try {
+			return l1.getfirst();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			return e.hashCode();
+		}
+	}
+	public void display()
+	{
+		l1.display();
+	}
+
+}
