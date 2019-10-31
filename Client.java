4f072411402f3b0b6da536d84a87f784dd450c2d
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-10-31 11:57:40 +0530

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,3 +1,6 @@
+package july27;
+
+import june21.finddata;
 
 public class Client {
 
@@ -5,37 +8,46 @@ public class Client {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] pre={50, 25, 12,37, 75, 62,57}; 
-		int[] post={12, 37, 25, 62, 57, 75, 50}; 
-		int[] in={12, 25,37, 50, 62, 75,57}; 
+		// TODO Auto-generated method stub
+		int[] pre={50, 25, 12, 37, 75, 62, 87}; 
+		int[] post={12, 37, 25, 62, 87, 75, 50}; 
+		int[] in={12, 25, 37, 50, 62, 75, 87}; 
+		int[] sa={12,25,37,50,62,75,87};
 		// TODO Auto-generated method stub
 //		50 true 25 true 12 false false true 37 false false true 75 true 62 false false true 57 false false 
-		BinaryTree bt=new BinaryTree(pre,in);
-		bt.display();
-//		System.out.println(bt.size2());
-//		System.out.println(bt.find(62));
-//		System.out.println(bt.max());
-//		System.out.println(bt.height());
-//        System.out.println(bt.diameter());
-//        System.out.println(bt.diameter2());
-//        System.out.println(bt.isBalanced());
-//        System.out.println(bt.bst());
-//        bt.preorder();
+		BST bst=new BST(sa);
+		bst.display();
+//		System.out.println(bst.size2());
+//		System.out.println(bst.find(62));
+//		System.out.println(bst.max());
+//		System.out.println(bst.height());
+//		System.out.println(bst.min());
+//		bst.add(60);
+//		bst.display();
+//        System.out.println(bst.diameter());
+//        System.out.println(bst.diameter2());
+//        System.out.println(bst.isBalanced());
+//        bst.preorder();
 //        System.out.println();
-//        bt.postorder();
+//        bst.postorder();
 //        System.out.println();
-//        bt.inorder();
+//        bst.inorder();
 //        System.out.println();
-//        bt.levelorder();
+//        bst.levelorder();
 //        System.out.println();
-//        bt.iterativePreOrder();
+//        bst.iterativePreOrder();
 //        System.out.println();
-//        bt.iterativePostOrder();
+//        bst.iterativePostOrder();
 //        System.out.println();
-//        bt.iterativeInOrder();
-//		bt.printWithoutSibling();
-//		bt.removeLeaves();
-//		bt.display();
-		bt.bstLarger();
+//        bst.iterativeInOrder();
+//		bst.remove(75);
+//		bst.display();
+//		bst.replaceWithSumOfLargerNum();
+//		bst.display();
+//		bst.printInOrder(37, 62);
+		System.out.println(bst.sumOfTwoNodes(50));
 	}
-}
+
+	}
+
+
